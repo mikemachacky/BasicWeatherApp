c0bfdb0ac4dc7043f444a14200686dbee52dd1dc
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-02-15 19:46:28 +0100

    communityToolkit.Maui added
    Fixed the StatusBar problem :smile:

--- BasicWeatherApp/MauiProgram.cs
+++ BasicWeatherApp/MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-
+using CommunityToolkit.Maui;
 namespace BasicWeatherApp
 {
     public static class MauiProgram
@@ -9,7 +9,8 @@ namespace BasicWeatherApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+                .UseMauiCommunityToolkit()
+                            .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
